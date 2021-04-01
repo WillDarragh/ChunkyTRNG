81c6d16a9292e5057c9b50c6343135e9b4f2a29d
Will Darragh <wdarragh16@gmail.com>
2021-03-31 21:04:56 -0400

    More testing with rp2040

--- RP2040/code.py
+++ RP2040/code.py
@@ -1,15 +1,34 @@
 # Placeholder code for testing
 
+# Standard Library
 import time
+
+# CircuitPython Core Modules
 import board
+import digitalio
 import rp2pio
+import supervisor
+
+# Adafruit CircuitPython Library
 import adafruit_pioasm
 
+# Dim neopixel
+supervisor.set_rgb_status_brightness(6)
+
+# Simple PIO to read from a pin
 hello = """
-.program hello
 in pins, 32
 """
 
+# LED for testing
+led = digitalio.DigitalInOut(board.LED)
+led.direction = digitalio.Direction.OUTPUT
+
+while True:
+    print(1)
+    time.sleep(0.001)
+
+hello_program = """
 assembled = adafruit_pioasm.assemble(hello)
 
 sm = rp2pio.StateMachine(
@@ -26,3 +45,4 @@ for i in range(100000):
     sm.readinto(reading)
 
 print('done')
+"""
